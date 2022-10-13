21816a1fe05cd814ba0d24eda39c04cca705df44
paul9748 <paul9748@hotmail.co.kr>
2022-10-14 01:36:01 +0900

    media upload test

--- back/api/settings.py
+++ back/api/settings.py
@@ -15,9 +15,9 @@ ssh_tunnel = SSHTunnelForwarder(
 ssh_tunnel.start()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/img/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
