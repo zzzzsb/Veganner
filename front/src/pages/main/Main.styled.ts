03f713c9c3066046d0679ffdd53e9a1ed43d0d4b
zzzzsb <ji12subin@gmail.com>
2022-10-17 01:14:44 +0900

    feat: add 2nd page

--- front/src/pages/main/Main.styled.ts
+++ front/src/pages/main/Main.styled.ts
@@ -2,6 +2,10 @@ import styled from "styled-components";
 import main1 from "../../assets/img/메인1.png";
 import button1 from "../../assets/img/btn_동참하기.png";
 
+export interface MainStyledProps {
+  height?: number;
+  backgroundImg?: string;
+}
 export const Main1Layout = styled.div`
   display: flex;
   width: auto;
@@ -36,4 +40,51 @@ export const Button1 = styled.div`
   background-repeat: no-repeat;
   float: right;
   margin-top: 15px;
+`;
+
+export const PageLayout = styled.div<MainStyledProps>`
+  width: auto;
+  height: ${(props) => `${props.height}px`};
+  font-family: "Noto Sans KR";
+  text-align: center;
+`;
+
+export const RowTextBox = styled.div`
+  height: 150px;
+  text-align: left;
+  margin: 20px 100px;
+  color: #212121;
+`;
+
+export const MediumTitleText = styled.div`
+  font-size: 40px;
+  font-weight: 800;
+  text-align: left;
+  margin: 18px 0;
+`;
+
+export const Text = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  text-align: left;
+  margin: 10px auto;
+`;
+
+export const ChartBox = styled.div`
+  width: 450px;
+  height: 400px;
+  text-align: center;
+  background-color: #ffffff;
+  border-radius: 15px;
+`;
+
+export const ImageBox = styled.div<MainStyledProps>`
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  background-image: ${(props) => `url(${props.backgroundImg})`};
+  display: flex;
+
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
--- front/src/pages/main/Main.tsx
+++ front/src/pages/main/Main.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import * as S from "../main/Main.styled";
+
 function Main() {
   return (
-    <S.Main1Layout>
-      <S.Main1Box>
-        <S.Text1>나와 지구를 위한 채식 문화,</S.Text1>
-        <S.Text1>비거너와 함께 시작해요!</S.Text1>
-        <S.Button1 />
-      </S.Main1Box>
-    </S.Main1Layout>
+    <>
+      <S.Main1Layout>
+        <S.Main1Box>
+          <S.Text1>나와 지구를 위한 채식 문화,</S.Text1>
+          <S.Text1>비거너와 함께 시작해요!</S.Text1>
+          <S.Button1 />
+        </S.Main1Box>
+      </S.Main1Layout>
+
+      <S.PageLayout height={800}>
+        <S.MediumTitleText>“오늘도 고기 드셨나요?”</S.MediumTitleText>
+        <S.Text>일주일에 하루만 고기를 먹지 않아도</S.Text>
+        <S.Text>
+          자동차가 1.6km를 달리면서 뿜어내는 것과 동일한 온실가스를 감축할 수
+          있어요.
+        </S.Text>
+        <S.ChartBox>
+          <S.ImageBox
+            backgroundImg={"../../assets/img/data/chart_1.png"}
+          ></S.ImageBox>
+        </S.ChartBox>
+      </S.PageLayout>
+    </>
   );
 }
 
