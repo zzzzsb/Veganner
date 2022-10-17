3c26e5b7d2eccdfa1adc875c7e4f29e686a42f9b
jk <puremint02s@naver.com>
2022-10-17 16:09:46 +0900

    Feat:로그아웃 추가 및 목록조회 추가

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import {useState} from "react";
 import * as S from "./Header.styled";
+import userState from "../../atoms/user";
+import { useRecoilValue, useResetRecoilState, useRecoilState } from "recoil";
+import * as Api from "../../api/api";
+
 
 // interface HeaderProps {
 //   src: string;
 // }
 
 function Header() {
+
+  const userReset=useResetRecoilState(userState);
+  const user = useRecoilValue(userState);
+
+  const  handleLogout = async () => {
+    
+
+    try {
+      // "user/login" 엔드포인트로 post요청함.
+      await Api.post("logout/");
+      // 유저 정보는 response의 data임.
+      userReset();
+      // 기본 페이지로 이동함.
+    
+    } catch (err) {
+      console.log("로그아웃 실패하였습니다.\n", err);
+    }
+  };
+ 
+
+  // const isLogin = !!userState.user;
   return (
     <div>
       <S.HeaderWrapper>
@@ -14,7 +40,19 @@ function Header() {
           <S.StyledLink to="/insights">Insights.</S.StyledLink>
           <S.StyledLink to="/share">Share.</S.StyledLink>
           <S.StyledLink to="/explore">Explore.</S.StyledLink>
-          <S.StyledLink to="/login">Register.</S.StyledLink>
+          
+          {user === null ? (
+            <S.StyledLink to="/login">Login/Register.</S.StyledLink>
+                      
+                    ) : (
+                        <>
+                            <S.StyledLink to="/" onClick={handleLogout}>logout</S.StyledLink>
+                            
+                        </>
+                    )}
+
+
+          
         </S.Navbar>
       </S.HeaderWrapper>
     </div>
--- front/src/pages/view/View.tsx
+++ front/src/pages/view/View.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import * as S from "../view/View.styled";
+import Search from "../view/Search";
+import List from "../view/List";
+
+
 
 function View() {
   return (
-    <S.Layout>
-      <S.TitleLayout>
-        <S.Label>레시피</S.Label>
-        <S.Title>토마토 파프리카 그린 샐러드</S.Title>
-      </S.TitleLayout>
-      <S.InfoLayout>
-        <S.PostInfo>
-          <S.UserPic></S.UserPic>
-          <S.UserName>김현정</S.UserName>
-          <S.InfoLine1></S.InfoLine1>
-          <S.PostDate>2022-10-10</S.PostDate>
-          <S.InfoLine2></S.InfoLine2>
-          <S.PostCate></S.PostCate>
-        </S.PostInfo>
-        <S.Like>
-          <S.LikeIcon></S.LikeIcon>
-          <S.LikeCount>0</S.LikeCount>
-        </S.Like>
-      </S.InfoLayout>
-    </S.Layout>
+    <>
+    <Search/>
+    <List/>
+    </>
   );
 }
 
