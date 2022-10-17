df34f489fbf4c256871888be3368da8847cd62b4
jk <puremint02s@naver.com>
2022-10-17 20:08:17 +0900

    Feat:search 코드 수정

--- front/src/pages/list/List.tsx
+++ front/src/pages/list/List.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import * as Api from "../../api/api";
 import styled from "styled-components";
+import Search from "./Search"
 
 function useViewList() {
   const [lists, setLists] = useState([]);
@@ -12,12 +13,16 @@ function useViewList() {
   }, []);
 
   return (
+    <>
+    <Search/>
+
     <div>
       {lists.map((list, index) => {
         console.log(list);
         return <div key={index}>{list["Title"]}</div>;
       })}
     </div>
+    </>
   );
 }
 export default useViewList;
--- front/src/pages/list/Search.tsx
+++ front/src/pages/list/Search.tsx
@@ -1,49 +1,77 @@
 /*검색창*/
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import * as Api from "../../api/api";
 
+function ListSearch() {
+  const [lists, setLists] = useState([]);
+  const [search, setSearch] = useState("");
+  const [currentPosts, setCurrentPosts] = useState([]);
 
-function MountainSearch() {
-  const [posting, setPosting] = useState("");
-
+  useEffect(() => {
+    const userData = async () => {
+      await Api.get("board").then((res) => {
+        setLists(res.data.patientList);
+        // setCurrentPosts(res.data.patientList.slice(indexOfFirstPost, indexOfLastPost))
+      });
+    };
+    userData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       // "registration" 엔드포인트로 post요청함.
 
-      const res = await Api.get("board/");
-      setPosting(res.data);
-      console.log(res.data);
+      if (search === null || search === "") {
+        //검색어가 없을 경우 전체 리스트 반환
+        await Api.get("board").then((res) => {
+          setLists(res.data.userList);
+        });
+      } else {
+        //검색 구현
+        const filterData = lists.filter((row) => row['Title'].includes(search));
+        setLists(filterData);
+      }
+
+      // setPosting(res.data);
+      // console.log(res.data);
     } catch (err) {
       console.log("목록조회에 실패하였습니다.", err);
     }
-    setPosting("");
+    setSearch("");
   };
 
-//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-//     setValue(e.target.value);
-//     onChange(e.target.value);
-//   };
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
 
-//   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     // 검색 창에 입력된 글자를 받아오는 함수
-//     setSearch(e.target.value);
-//   };
+  //   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //     setValue(e.target.value);
+  //     onChange(e.target.value);
+  //   };
+
+  //   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     // 검색 창에 입력된 글자를 받아오는 함수
+  //     setSearch(e.target.value);
+  //   };
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="검색"
         autoFocus={true}
-        // value={value || ""}
-        // onChange={handleChange}
+        value={search}
+        onChange={onChangeSearch}
       />
-      
-      <button/>검색<button/>
+      <button type='submit'>검색</button>
     </form>
+    </>
   );
 }
-export default MountainSearch;
+
+export default ListSearch;
