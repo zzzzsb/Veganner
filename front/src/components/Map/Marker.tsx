ee2669a0ae8410f240e241dd976dab7e78684bd3
Ryn <ryn1501@gmail.com>
2022-10-18 05:31:13 +0900

    Style: css

--- front/src/components/Map/Marker.tsx
+++ front/src/components/Map/Marker.tsx
@@ -1,30 +1,23 @@
-import {MapMarker} from 'react-kakao-maps-sdk'
+import { MapMarker } from "react-kakao-maps-sdk";
 import { Restaurant } from "../../types/restaurant";
 interface Markerprops {
-    location: Restaurant;
+  location: Restaurant;
 }
 
-function Marker({location}: Markerprops){
-    return (
-    // location.map((position: any, index: number) => (
-        <MapMarker
-        // key={i}
-        position={{lat: location.y, lng: location.x}}
-        image={{
-            // 이미지 왜 오류 이미지 뜨지?
-            src: "./assets/image/marker_icon-icons.com_54388.png",
-            size: {
-            width: 24,
-            height: 35
-            }
-        }}
-        title = {location.name}
-        
-        
-        />
-    )
-};
-  
-
+function Marker({ location }: Markerprops) {
+  return (
+    <MapMarker
+      position={{ lat: location.y, lng: location.x }}
+      image={{
+        src: "../assets/image/marker_icon-icons.com_54388.png",
+        size: {
+          width: 24,
+          height: 35,
+        },
+      }}
+      title={location.name}
+    />
+  );
+}
 
 export default Marker;
