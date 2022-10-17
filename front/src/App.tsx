f67a4c0212f6496cbba94cfa7564eb9fffcfeeea
김현정 <khjung11203@gmail.com>
2022-10-17 15:36:18 +0900

    fix: fix conflicts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,11 +4,6 @@ import { Reset } from "styled-reset";
 import Header from "./components/header/Header";
 import Main from "./pages/main/Main";
 import Insights from "./pages/Insights";
-<<<<<<< HEAD
-import View from "./pages/view/View";
-import "./styles/styles.css";
-import { Reset } from "styled-reset";
-=======
 import Share from "./pages/Share";
 import Write from "./pages/write/Write";
 import LoginForm from "./pages/login/Login";
@@ -16,7 +11,6 @@ import RegisterForm from "./pages/register/Register";
 import View from "./pages/view/View";
 import Map from "./pages/Map";
 import { IEditor } from "./pages/write/Write";
->>>>>>> origin/dev
 
 function App() {
   return (
@@ -26,11 +20,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-<<<<<<< HEAD
-          <Route path="/insights" element={<Insights />} />
-          <Route path="/share" element={<View />} />
-          {/* <Route path="/login" element={<LoginForm />} /> */}
-=======
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/insights" element={<Insights />} />
@@ -39,7 +28,6 @@ function App() {
           <Route path="/write" element={<Write />} />
           <Route path="/explore" element={<Map />} />
           <Route path="/write" element={<Write />} />
->>>>>>> origin/dev
         </Routes>
       </BrowserRouter>
     </div>
