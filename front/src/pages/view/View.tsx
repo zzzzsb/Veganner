00a153bd94eebc6d0ba9e1f707dd8a9b15cdbb55
김현정 <khjung11203@gmail.com>
2022-10-19 19:07:48 +0900

    feature: submit comment

--- front/src/pages/view/View.tsx
+++ front/src/pages/view/View.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../list/Search";
 import List from "../list/List";
 import { createGlobalStyle } from "styled-components";
@@ -6,6 +6,7 @@ import ViewTemplate from "./ViewTemplate";
 import ViewHead from "./ViewHead";
 import ViewContent from "./ViewContent";
 import ViewComment from "./ViewComment";
+import * as Api from "../../api/api";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,8 +14,41 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+function View({ match }: any) {
+  interface Post {
+    Type: string;
+    Title: string;
+    Thumbnail?: string;
+    CreationTime?: string;
+    User: string;
+    Likes: number;
+    Hashtag?: string;
+  }
 
-function View() {
+  const [post, setPost] = useState<Post>({
+    Type: "",
+    Title: "",
+    Thumbnail: "",
+    User: "",
+    Likes: 0,
+    Hashtag: "#",
+  });
+
+  async function getPost(id: string) {
+    try {
+      const res = await Api.get(`board/${id}`);
+      console.log(res.data);
+      setPost(res.data);
+    } catch (err) {
+      console.log("글 불러오기를 실패했습니다.\n", err);
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log(match);
+  //   getPost(match.params.id);
+  // }, []);
+
   return (
     <>
       <GlobalStyle />
@@ -23,7 +57,6 @@ function View() {
         <ViewContent></ViewContent>
         <ViewComment></ViewComment>
       </ViewTemplate>
-
     </>
   );
 }
