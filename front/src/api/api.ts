41594bede1d759ae670d95935e7dab8fe5f9f554
jk <puremint02s@naver.com>
2022-10-21 03:56:04 +0900

    fix:충돌 수정

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -4,9 +4,9 @@ const backendPortNumber = "8000";
 // 로컬서버
 const localBaseUrl = "localhost";
 // 개발서버
-const realBaseUrl = "kdt-ai5-team01.elicecoding.com";
+const realBaseUrl = "veganner-back.herokuapp.com";
 
-const serverUrl = `http://${localBaseUrl}:${backendPortNumber}/`;
+const serverUrl = `http://${realBaseUrl}/`;
 axios.defaults.withCredentials = true;
 async function get(endpoint: string, params = "") {
   console.log(
