605ecedb093f5399968ceaac51fce109615848e3
Jinwoo Kim <0126kjw@kakao.com>
2022-10-21 03:06:23 +0900

    fix: edit routes

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -6,7 +6,7 @@ const localBaseUrl = "localhost";
 // 개발서버
 const realBaseUrl = "kdt-ai5-team01.elicecoding.com";
 
-const serverUrl = `http://${localBaseUrl}:${backendPortNumber}/`;
+const serverUrl = `https://veganner-back.herokuapp.com/`;
 axios.defaults.withCredentials = true;
 async function get(endpoint: string, params = "") {
   console.log(
