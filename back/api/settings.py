913dba3b06daf3e6437d969e402772e9f4224be3
Jinwoo Kim <0126kjw@kakao.com>
2022-10-21 19:29:17 +0900

    fix: CORS

--- back/api/settings.py
+++ back/api/settings.py
@@ -118,10 +118,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api.wsgi.application'
 
 # CORS 권한 설정
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
-                         'http://localhost:3000',
-                         'http://kdt-ai5-team01.elicecoding.com:3000',
-                         'http://kdt-ai5-team01.elicecoding.com:80']
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
+#                          'http://localhost:3000',
+#                          'http://kdt-ai5-team01.elicecoding.com:3000',
+#                          'http://kdt-ai5-team01.elicecoding.com:80']
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 # Database
