5027746539b957a372f210ec2bbd692f9bf761cf
jk <puremint02s@naver.com>
2022-10-22 10:50:55 +0900

    fix:카카오오류

--- front/src/pages/login/Kakao.tsx
+++ front/src/pages/login/Kakao.tsx
@@ -34,7 +34,7 @@ function KakaoLogin() {
         if (data.access_token) {
           localStorage.setItem("token", data.access_token);
 
-          fetch(`http://kdt-ai5-team01.elicecoding.com/kakao/login/`, {
+          fetch(`http://kdt-ai5-team01.elicecoding.com:5000/kakao/login/`, {
             credentials: "include",
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -44,7 +44,7 @@ function KakaoLogin() {
           }).then((res) => {
             localStorage.clear();
             window.sessionStorage.setItem("email", "kakao");
-            // setUser({ email: "kakao", password: "" });
+            setUser({ email: "kakao.com", password: "" });
             navigate("/", { state: pathname });
           });
         } else {
